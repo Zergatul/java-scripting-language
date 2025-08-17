58ca9cec68ea78651e31550501ca9d605b5d6bd9
Igor Budzhak <z3rgatul@gmail.com>
2025-08-17 19:20:27 +0300

    clas method decl

--- src/main/java/com/zergatul/scripting/binding/ClassMethodDeclaration.java
+++ src/main/java/com/zergatul/scripting/binding/ClassMethodDeclaration.java
@@ -6,15 +6,21 @@ import com.zergatul.scripting.symbols.SymbolRef;
 
 public class ClassMethodDeclaration extends Declaration {
 
+    private final boolean isAsync;
     private final BoundTypeNode typeNode;
     private final BoundParameterListNode parameters;
     private final boolean hasError;
 
-    public ClassMethodDeclaration(String name, SymbolRef symbolRef, BoundTypeNode typeNode, BoundParameterListNode parameters, boolean hasError) {
+    public ClassMethodDeclaration(String name, SymbolRef symbolRef, boolean isAsync, BoundTypeNode typeNode, BoundParameterListNode parameters, boolean hasError) {
         super(name, symbolRef);
+        this.isAsync = isAsync;
         this.typeNode = typeNode;
         this.parameters = parameters;
         this.hasError = hasError;
+    }
+
+    public boolean isAsync() {
+        return isAsync;
     }
 
     public BoundTypeNode getTypeNode() {
